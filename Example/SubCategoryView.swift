1ea330ef64f09bb40630faf7c3beac414b153b6c
khara <khara@mercari.com>
2017-10-20 23:03:59 +0900

    微調整

--- Example/SubCategoryView.swift
+++ Example/SubCategoryView.swift
@@ -14,7 +14,6 @@ class SubCategoryView: UIView {
 
     static func make(withFrame: CGRect, mainCategory: MainCategory, whenClosed: (() -> Void)?) -> SubCategoryView {
         let subCategoryView = SubCategoryView(frame: withFrame)
-        subCategoryView.backgroundColor = UIColor.clear
 
         subCategoryView.magneticView = MagneticView(frame: withFrame)
         subCategoryView.addSubview(subCategoryView.magneticView)
@@ -26,14 +25,16 @@ class SubCategoryView: UIView {
     }
 
     private func setup(withMainCategory: MainCategory) {
+        self.backgroundColor = UIColor.clear
+
         self.magneticView.magnetic.backgroundColor = UIColor(red: 66/255, green: 66/255, blue: 66/255, alpha: 0.74)
         self.magneticView.backgroundColor = UIColor.clear
 
         let radius = CGFloat(Int(withMainCategory.frame.width / 2) - 1) // Node.init 内で physicsBody を作る時に入力の radius に +2 しているのの補正
         let selectedCategory = MainCategory(text: withMainCategory.text, radius: radius)
         selectedCategory.isSelected = true
-        let center = CGPoint(x: withMainCategory.frame.midX, y: withMainCategory.frame.midY)
         selectedCategory.physicsBody?.isDynamic = false
+        let center = CGPoint(x: withMainCategory.frame.midX, y: withMainCategory.frame.midY)
 
         self.magneticView.magnetic.magneticField.position = center
         self.magneticView.magnetic.addChild(selectedCategory)
